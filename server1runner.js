ea7390d46da779a34d00f268bd60f5177bdd1037
CodeforAbj <codeforAbj@gmail.com>
2024-07-05 16:34:37 +0530

    Updated Respective File structures

--- server1runner.js
+++
@@ -1,6 +0,0 @@
-import { createServer } from "../server.js";
-import serverConfig from "../serversConfig";
-createServer(
-  serverConfig.serverList[0].hostName,
-  serverConfig.serverList[0].port
-);
--- src/serverRunners/server4runner.js
+++ src/serverRunners/server4runner.js
@@ -1,5 +1,5 @@
-import { createServer } from "./server.js";
-import serverConfig from "./src/serversConfig";
+import { createServer } from "../server.js";
+import serverConfig from "../serversConfig.js";
 createServer(
   serverConfig.serverList[3].hostName,
   serverConfig.serverList[3].port
